52e1b22c121c35c15f0f0ffd31a253598ecab5fd
LajiYang <842458343@qq.com>
2017-04-09 14:46:59 +0800

    8888

--- 2048.js
+++ 2048.js
@@ -259,19 +259,19 @@ function t(event){
                           y0=y;
         case "touchmove":
             var jdz=Math.abs((y-y0)/(x-x0))
-            if(((y-y0)>0)&&(!(x-x0)||(jdz>1)))
+            if(((y-y0)>0)&&((!(x-x0))||(jdz>1)))
             {
                 fx="down"; //up
             }
-            else if((y-y0)<0&&(!(x-x0)||(jdz>1)))
+            else if((y-y0)<0&&((!(x-x0))||(jdz>1)))
             {
                 fx="up";  //上
             }
-            else if((x-x0)>0&&(!(y-y0)||(jdz<1)))
+            else if((x-x0)>0&&((jdz<1)))
             {
                 fx="right";   //右
             }
-            else if((x-x0)<0&&(!(y-y0)||(jdz<1)))
+            else if((x-x0)<0&&((jdz<1)))
             {
                 fx="left";    //左
             }
